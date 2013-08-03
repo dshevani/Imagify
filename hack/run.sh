27f2f4ea03d1643156ed5dfb74cb6090d12e1c13
zerone <zerone@JayaprakashPC.(none)>
2013-08-04 03:38:01 +0530

    Initial Commit

---
+++ hack/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -o orig_senna -O3 -ffast-math SENNA_CHK.c SENNA_Hash.c SENNA_main.c SENNA_NER.c SENNA_nn.c SENNA_POS.c SENNA_PT0.c SENNA_SRL.c SENNA_Tokenizer.c SENNA_utils.c SENNA_VBS.c SENNA_CHK.h SENNA_Hash.h SENNA_NER.h SENNA_nn.h SENNA_POS.h SENNA_PT0.h SENNA_SRL.h SENNA_Tokenizer.h SENNA_utils.h SENNA_VBS.h
